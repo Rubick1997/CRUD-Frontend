f5dc527f1e45e72301ebe3362b57903805c86723
Rustam <rustam905@gmail.com>
2021-02-22 17:54:28 -0800

    Table was made

--- src/MainComponent.js
+++ src/MainComponent.js
@@ -1,10 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Main = () => {
 	const [firstName, setFirstName] = useState("");
 	const [lastName, setLastName] = useState("");
 	const [email, setEmail] = useState("");
+
+	const [userList, setUserList] = useState([]);
+
+	useEffect(() => {
+		axios.get("http://localhost:3001/read").then((response) => {
+			console.log(response.data);
+			setUserList(response.data);
+		});
+	}, []);
 
 	const addToDataBase = () => {
 		axios.post("http://localhost:3001/insert", {
@@ -36,6 +45,27 @@ const Main = () => {
 				onChange={(event) => setEmail(event.target.value)}
 			/>
 			<button onClick={addToDataBase}>Add to database</button>
+			<h1>Users List</h1>
+			{userList && (
+				<table className='table'>
+					<thead>
+						<tr>
+							<th scope='col'>First Name</th>
+							<th scope='col'>Last Name</th>
+							<th scope='col'>Email</th>
+						</tr>
+					</thead>
+					<tbody>
+						{userList.map((user) => (
+							<tr key={user.id}>
+								<td>{user.userName}</td>
+								<td>{user.userLastName}</td>
+								<td>{user.userEmail}</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			)}
 		</div>
 	);
 };
